1604df73b4851860f02a87f6e6f7a47efdd9837f
Resley Rodrigues <rslyrulz@yahoo.co.in>
2017-08-25 20:26:09 +0100

    Added options for negative numbers, max decimal places & distance between commas

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Comma Separated Number
-JQuery plugin to convert any text input to a number input with comma separate display
+JQuery plugin to convert any text input to a number input with comma separate display.         
+Additionally it allows for certain characters to be entered as shortcuts for numbers.           
+For example, if the user types in `1k` it is converted to `1,000`.           
+To get the value without the comma use the `data-value` attribute.                  
 
 ### Basic usage
 ```html
-<input type="text">
+<input type="text" value="12k>
 ```
 ```javascript
 $('input').commaSeparatedNumber()
@@ -13,10 +16,23 @@ $('input').commaSeparatedNumber({
 	allowedChars: "bmk",
 	allowDecimals: true
 })
+
+parseFloat($('input').data('value')) // 12000
 ```
 ### Options
 
 |Option|Description|Possible values|Default|                   
 |======|===========|===============|=======|                    
-|allowedChars| String of allowed chars| strings made up of b, m, k| "bmk"|              
-|allowDecimal| Allow decimal numbers to be input| true, false| true|          
+|allowedChars| String of allowed chars which are auto replaced with numbers| strings made up of b, m, k| `"bmk"`|              
+|allowDecimal| Allow decimal numbers to be input| true, false| `true`|          
+|allowNegative| Allow negative numbers to be input| true, false| `true`|          
+|maxDecimals| Maximum digits allowed after the decimal point. (applicable only if `allowDecial` is `true`| int or null| `null`|          
+|distanceBetweenCommas| The number of digits after which a comma must be added| int | `3`|          
+
+**Note:** The letter k, m & b are replaced with `000` (thousands), `000000` (millions) & `000000000` (billions) respectively.                 
+
+
+### Contributing
+
+Please feel free to add to this project, specially if you can write tests.      
+I am not a JavaScript developer and can't get my tests running, but plugin works fine when tested manually.
--- src/comma_separated_number.ts
+++ src/comma_separated_number.ts
@@ -1,10 +1,13 @@
 class CommaSeparatedNumber {
-    element: JQuery
+    element: Element
 	options: object
 	
 	const defaults: object = {
 		allowedChars: "bmk",
-		allowDecimal: true
+		allowDecimal: true,
+		allowNegative: true,
+		maxDecimals: null,
+		distanceBetweenCommas: 3
 	}
 	constructor(element, options = {}) {
         this.element = element
@@ -17,12 +20,21 @@ class CommaSeparatedNumber {
 	
 	updateOptions(options) {
 		this.options = $.extend(this.options, this.defaults, options)
-		var regexString = "[^\\d," 
+		var regexString = "[^\\d"  + this.options.allowedChars
 		if (this.options.allowDecimal) {
 			regexString += "."
 		}
-		regexString += this.options.allowedChars + "]"
+		if (this.options.allowNegative) {
+			regexString += "-"
+		}
+		regexString += "]"
 		this.regex = new RegExp(regexString, "i")
+
+		regexString = "\\B(?=(\\d{" + this.options.distanceBetweenCommas + "})+(?!\\d))"
+		this.commaRegex = new RegExp(regexString, "g")
+
+		regexString = "(-?\\d+(?:\\.\\d{" + this.options.maxDecimals + "}))\\d*"
+		this.maxDecimalsRegex = new RegExp(regexString)
 	}
     
 	validateInput(e) {
@@ -31,29 +43,49 @@ class CommaSeparatedNumber {
             : e.originalEvent.clipboardData.getData("Text")
 
         // if the input is invalid, just discard it
-		if(this.regex.test(char) || causesMultipleDecimals(char)) {
-      e.preventDefault()
-      e.stopImmediatePropagation()
-
+		if(this.isInputInvalid(char) {
+			e.preventDefault()
+			e.stopImmediatePropagation()
 		}
     }
 
-	causesMultipleDecimals(char) {
-	return /.*\..*\..*/.test($(this.element).val() + char)
+	isInputInvalid(char) {
+		if(this.regex.test(char) {
+			return true
+		}
+		switch(char) {
+			case ".":  return this.causesMultipleDecimals()
+			case "-": return !this.isValidNegativeSign()
+			default: return false
+		}
 	}
+
+	causesMultipleDecimals() {
+		return /.*\..*\..*/.test($(this.element).val() + ".")
+	}
+
+	isValidNegativeSign() {
+		return this.element.selectionStart === 0 && this.element.value.substr(0, 1) !== "-"
+	}
+
     cleanInput() {
         var selectionStart: number = this.element.selectionStart
         var selectionEnd: number = this.element.selectionEnd
-        var value: string = $(this.element).val()
+        var value: string = this.element.value
         var oldLength: number = value.length
         value = value
-            .replace(/,/g, '')                        // remove previously added commas
+            .replace(/,/, '')                         // remove previously added commas
             .replace(/b/gi, 'mk')                     // replace b with mk so that it becomes 9 zeros in the next step
             .replace(/m/gi, 'kk')                     // replace m with kk so that it becomes 6 zeros in the next step
             .replace(/k/gi, '000')                    // replace each k with 3 zeros
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")    // add a comma in the position of 3 digits having 1 digit before them
+		if (this.options.allowDecimal && this.options.maxDecimals !== null) {
+			value = value.replace(this.maxDecimalsRegex, "$1")    // truncate to maxDecimals decimal places
+		}
+		
+		this.element.setAttribute('data-value', value)
+		value = value.replace(this.commaRegex, ",")           // add a comma before every distanceBetweenCommas digits
         var diff: number = value.length - oldLength
-        $(this.element).val(value)
+        this.element.value = value
         this.element.setSelectionRange(selectionStart + diff, selectionEnd + diff)
     }
 }
